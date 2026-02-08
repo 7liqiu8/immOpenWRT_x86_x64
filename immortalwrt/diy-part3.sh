833a511c12d3245d73cc8ff22b325f5c8dcd63da
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-02-08 22:49:57 +0800

    Update diy-part3.sh

--- immortalwrt/diy-part3.sh
+++ immortalwrt/diy-part3.sh
@@ -1,1 +1,5 @@
-#1
+#!/bin/bash
+#
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
+sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
